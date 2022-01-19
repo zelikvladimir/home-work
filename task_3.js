27ec4aa6b430daf681491462b05777a8c0bcc994
Vladimir Zelik <kazantip81v@gmail.com>
2022-01-19 15:22:09 +0200

    added task-3

---
+++ task_3.js
@@ -0,0 +1,9 @@
+let val = "z";
+
+if (val === "a") {
+    console.log(val);
+} else if (val === "b" || val === "c" || val === "d" || val === "e") {
+    console.log("others");
+} else {
+    console.log("unknown");
+}
