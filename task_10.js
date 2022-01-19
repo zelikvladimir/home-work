f148b2c6972ef08693d143b4a5dc06b1ae161ec1
Vladimir Zelik <kazantip81v@gmail.com>
2022-01-19 16:49:44 +0200

    added task 9 & 10

---
+++ task_10.js
@@ -0,0 +1,19 @@
+let arr = [1, 6, 3, 5, 4, 2];
+
+for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+    let wasExchange = false;
+
+    for (let j = 0, endJ = endI - i; j < endJ; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            wasExchange = true;
+        }
+    }
+
+    if (!wasExchange) break;
+}
+
+console.log(arr);
