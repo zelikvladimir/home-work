b1457474f0ae90419f43d9a735740e3f29d6a3d2
Vladimir Zelik <kazantip81v@gmail.com>
2022-01-19 15:31:41 +0200

    added task-4

---
+++ task_4.js
@@ -0,0 +1,19 @@
+let val = 3;
+
+switch (val) {
+  case 0:
+      console.log('0');
+      break;
+
+  case 1:
+      console.log('1');
+      break;
+
+  case 2:
+  case 3:
+      console.log('2,3');
+      break;
+  default:
+      console.log('unknown');
+
+}
