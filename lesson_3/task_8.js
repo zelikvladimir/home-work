aa1f7c3ef7f02e9ac72f07ec66bbcb5dc3e272c9
Vladimir Zelik <kazantip81v@gmail.com>
2022-01-21 11:11:22 +0200

    fix task 8

--- lesson_3/task_8.js
+++ lesson_3/task_8.js
@@ -3,10 +3,11 @@ console.log(printArr([1,2,3,4]));
 function printArr(arr) {
     if (!Array.isArray(arr)) throw new Error('parameter type is not Array');
 
-    let firstItem = arr.splice(0, 1);
-
-    console.log(firstItem[0]);
     if (arr.length) {
+        let firstItem = arr.splice(0, 1);
+
+        console.log(firstItem[0]);
+
         return printArr(arr);
     }
 }
